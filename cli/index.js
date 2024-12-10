c4ac22e431ad99b94ddf44c327a79eaefdfc2dd6
Simon Haines <nimon@hotmail.co.uk>
2024-12-10 16:38:26 +0000

    adds a new controller

--- cli/index.js
+++ cli/index.js
@@ -1,8 +1,10 @@
 import { program } from 'commander'
 import transactionController from './transaction.js'
+import currencyController from './currency.js'
 
 program.version('0.1.0').description('A Support Bank CLI')
 
 program.addCommand(transactionController)
+program.addCommand(currencyController)
 
 program.parse(process.argv)
--- cli/transaction.js
+++ cli/transaction.js
@@ -1,5 +1,7 @@
 import { Command } from 'commander'
 
+import Currency from '../models/Currency.js'
+
 const transactionController = new Command('transaction')
 
 transactionController
@@ -7,6 +9,14 @@ transactionController
   .description('Log transaction data to the console')
   .action((from, to, amount) => {
     console.log(from, to, amount)
+  })
+
+transactionController
+  .command('split <amount> <code> <people>')
+  .description('Split a bill between multiple people')
+  .action((amount, code, people) => {
+    const currency = new Currency(code || 'USD', amount / people)
+    console.log(currency.toString())
   })
 
 export default transactionController
